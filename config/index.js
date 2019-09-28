785cabc165976b03807f8436cbc022273489a1be
xuyangyang <544832866@qq.com>
2019-09-28 13:07:15 +0800

    添加隐藏属性

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ let serverObj = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '192.168.1.12', // can be overwritten by process.env.HOST
+    host: '192.168.43.209', // can be overwritten by process.env.HOST
     port: 8000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
